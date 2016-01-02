0bbadd211f4cfa7ea222a3a17b33bddd3bfe6d70
morapost <morapost@gmail.com>
2016-01-03 00:02:57 +0530

    changed the data types as per instruction in Review3

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,14 +8,14 @@ var bio = {
         "github": "morapost",
         "location": "Bangalore,Karnataka"
     },
-    "picURL": "images/Bhushan.jpg",
-    "welmsg": "I have been working in PeopleSoft for past 5 years and I have huge intrest in web development. I want to develop the websites which help everyone connects digitally and makes each one of us as a global citizen.",
+    "biopic": "images/Bhushan.jpg",
+    "welcomeMessage": "I have been working in PeopleSoft for past 5 years and I have huge intrest in web development. I want to develop the websites which help everyone connects digitally and makes each one of us as a global citizen.",
     "skills": ["Cooking", "Programming", "Teaching", "JS", "Troubleshooting"],
     "display": function() {
         var formattedName = HTMLheaderName.replace("%data%", bio.name);
         var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
-        var formattedpic = HTMLbioPic.replace("%data%", bio.picURL);
-        var formattedwelmsg = HTMLWelcomeMsg.replace("%data%", bio.welmsg);
+        var formattedpic = HTMLbioPic.replace("%data%", bio.biopic);
+        var formattedwelmsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage);
 
         $("#header").prepend(formattedRole).prepend(formattedName).append(formattedpic, formattedwelmsg);
         $("#header").append(HTMLskillsStart);
@@ -44,28 +44,28 @@ bio.display();
 
 var work = {
     "jobs": [{
-        "Employer": "Oracle",
-        "Title": "Sr Consultant",
-        "Dates": "Oct-26-2015",
+        "employer": "Oracle",
+        "title": "Sr Consultant",
+        "dates": "Oct-26-2015",
         "location": "Bangalore,Karnataka",
-        "Description": "PeopleSoft Product Developement on Various DB's and Platforms"
+        "description": "PeopleSoft Product Developement on Various DB's and Platforms"
     }, {
-        "Employer": "Cognizant",
-        "Title": "Consultant",
-        "Dates": "Mar-29-2010",
+        "employer": "Cognizant",
+        "title": "Consultant",
+        "dates": "Mar-29-2010",
         "location": "Chennai,Tamil Nadu",
-        "Description": "PeopleSoft Product Support on MS SQL server and Oracle"
+        "description": "PeopleSoft Product Support on MS SQL server and Oracle"
     }],
 
     "display": function() {
         var job = work.jobs.length;
         for (var i = 0; i < job; i++) {
             $("#workExperience").append(HTMLworkStart);
-            var formattedworkEmp = HTMLworkEmployer.replace("%data%", work.jobs[i].Employer);
-            var formattedworkTitle = HTMLworkTitle.replace("%data%", work.jobs[i].Title);
-            var formattedworkDates = HTMLworkDates.replace("%data%", work.jobs[i].Dates);
+            var formattedworkEmp = HTMLworkEmployer.replace("%data%", work.jobs[i].employer);
+            var formattedworkTitle = HTMLworkTitle.replace("%data%", work.jobs[i].title);
+            var formattedworkDates = HTMLworkDates.replace("%data%", work.jobs[i].dates);
             var formattedworkLoc = HTMLworkLocation.replace("%data%", work.jobs[i].location);
-            var formattedworkDesc = HTMLworkDescription.replace("%data%", work.jobs[i].Description);
+            var formattedworkDesc = HTMLworkDescription.replace("%data%", work.jobs[i].description);
             var formattedEmpTitle = formattedworkEmp + formattedworkTitle + formattedworkDates + formattedworkLoc + formattedworkDesc;
             $(".work-entry:last").append(formattedEmpTitle);
         }
@@ -76,34 +76,34 @@ var work = {
 work.display();
 
 var projects = {
-    "projwork": [{
-        "Title": "Meteor JS",
-        "Dates": "Nov-2015",
-        "Description": "Meteor JS Tutorial building sample Apps",
-        "image": "images/meteor.jpg"
+    "projects": [{
+        "title": "Meteor JS",
+        "dates": "Nov-2015",
+        "description": "Meteor JS Tutorial building sample Apps",
+        "images": ["images/meteor.jpg"]
     }, {
-        "Title": "Portfolio",
-        "Dates": "Dec-2015",
-        "Description": "Creating Portfolio Page using CSS and HTML",
-        "image": "images/portfolio.jpg"
+        "title": "Portfolio",
+        "dates": "Dec-2015",
+        "description": "Creating Portfolio Page using CSS and HTML",
+        "images": ["images/portfolio.jpg"]
     }, {
-        "Title": "Online Resume",
-        "Dates": "Dec-2015",
-        "Description": "Creating Online Resume using Javascript and Jquery",
-        "image": "images/resume.png"
+        "title": "Online Resume",
+        "dates": "Dec-2015",
+        "description": "Creating Online Resume using Javascript and Jquery",
+        "images": ["images/resume.png"]
     }],
     "display": function() {
 
-
-        for (var project in projects.projwork) {
+        var project = projects.projects.length;
+        for (var i = 0; i < project; i++) {
             $("#projects").append(HTMLprojectStart);
-            var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projwork[project].Title);
+            var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[i].title);
             $(".project-entry:last").append(formattedProjectTitle);
-            var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projwork[project].Dates);
+            var formattedProjectDates = HTMLprojectDates.replace("%data%", projects.projects[i].dates);
             $(".project-entry:last").append(formattedProjectDates);
-            var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projwork[project].Description);
+            var formattedProjectDescription = HTMLprojectDescription.replace("%data%", projects.projects[i].description);
             $(".project-entry:last").append(formattedProjectDescription);
-            var formattedProjectimage = HTMLprojectImage.replace("%data%", projects.projwork[project].image);
+            var formattedProjectimage = HTMLprojectImage.replace("%data%", projects.projects[i].images);
             $(".project-entry:last").append(formattedProjectimage);
 
 
@@ -117,16 +117,16 @@ var education = {
     "schools": [{
         "name": "PSNA College of Engineering",
         "location": "Dindigul,Tamil Nadu",
-        "Degree": "Bachelor of Engineering",
-        "Major": "Electronics & Communication",
-        "GradYear": "2005-2009",
-        "URL": "http://ieee.co.in"
+        "degree": "Bachelor of Engineering",
+        "majors": ["Electronics & Communication"],
+        "dates": "2005-2009",
+        "url": "http://ieee.co.in"
     }],
 
     "onlineCourses": [{
         "title": "Front End Nano Degree Web Developer",
         "school": "Udacity",
-        "GradYear": "2016",
+        "date": "2016",
         "url": "http://udacity.com"
     }],
 
@@ -136,10 +136,10 @@ var education = {
             $("#education").append(HTMLschoolStart);
 
             var formattedName = HTMLschoolName.replace("%data%", education.schools[i].name);
-            var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[i].Degree);
-            var formattedDates = HTMLschoolDates.replace("%data%", education.schools[i].GradYear);
+            var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[i].degree);
+            var formattedDates = HTMLschoolDates.replace("%data%", education.schools[i].dates);
             var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[i].location);
-            var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[i].Major);
+            var formattedMajor = HTMLschoolMajor.replace("%data%", education.schools[i].majors);
             $(".education-entry:last").append(formattedName + formattedDegree, formattedDates, formattedLocation, formattedMajor);
         }
         $("#education").append(HTMLonlineClasses);
@@ -149,24 +149,12 @@ var education = {
             $("#education").append(HTMLschoolStart);
             var formattedonlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[i].title);
             var formattedonlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[i].school);
-            var formattedonlineDate = HTMLonlineDates.replace("%data%", education.onlineCourses[i].GradYear);
+            var formattedonlineDate = HTMLonlineDates.replace("%data%", education.onlineCourses[i].date);
             var formattedURL = HTMLonlineURL.replace("%data%", education.onlineCourses[i].url);
             $(".education-entry:last").append(formattedonlineTitle + formattedonlineSchool, formattedonlineDate, formattedURL);
         }
     }
 };
 education.display();
-
-//Function to international Name format; Commented as it is not needed for this Project!
-// function inName(name) {
-//     name = name.trim().split(" ");
-//     name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
-//     name[1] = name[1].toUpperCase();
-//     var full_name = name[0] + " " + name[1];
-//     return full_name;
-// }
-
-// $("#main").append(internationalizeButton);
-
 
 $("#mapDiv").append(googleMap);
